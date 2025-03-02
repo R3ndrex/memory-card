41bda73bc7b3516d90c09a3c61e11f52d7bb3880
R3ndrex <yacholohchtoli@gmail.com>
2025-03-02 13:08:31 +0200

    add restart amount and working restart button

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,26 @@ import { useState } from "react";
 
 function App() {
     const [points, setPoints] = useState(0);
-    const pokemonAmount = 5;
+    const [restarts, setRestarts] = useState(0);
+    const pokemonAmount = 10;
+
     return (
         <>
             <header>
                 <h1>Pokemon Memory Game</h1>
                 <nav>
-                    <div>Points:{points}</div>
-                    <button>Restart</button>
+                    <div> Points:{points} </div>
+                    <div>Restarts:{restarts} </div>
+                    <button onClick={() => setRestarts((prev) => ++prev)}>
+                        Restart
+                    </button>
                 </nav>
             </header>
-            <PokemonCards setPoints={setPoints} pokemonAmount={pokemonAmount} />
+            <PokemonCards
+                setPoints={setPoints}
+                pokemonAmount={pokemonAmount}
+                restartAmount={restarts}
+            />
         </>
     );
 }
