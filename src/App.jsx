3ca1ad6465ffe6bed605929a9e0c3aeea5ec591e
R3ndrex <yacholohchtoli@gmail.com>
2025-03-02 15:48:00 +0200

    add nav with restart button, gamemode select, points and restart amount, add win screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
 import PokemonCards from "./components/PokemonCards";
+import WinnerScreen from "./components/WinnerScreen";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
     const [points, setPoints] = useState(0);
     const [restarts, setRestarts] = useState(0);
-    const pokemonAmount = 10;
+    const [pokemonAmount, setPokemonAmount] = useState(5);
+    const handleRestart = () => {
+        setRestarts((prev) => ++prev);
+        setPoints(0);
+    };
 
     return (
         <>
             <header>
                 <h1>Pokemon Memory Game</h1>
                 <nav>
-                    <div> Points:{points} </div>
-                    <div>Restarts:{restarts} </div>
-                    <button onClick={() => setRestarts((prev) => ++prev)}>
-                        Restart
-                    </button>
+                    <div>
+                        {" "}
+                        <div>{`Points: ${points}`}</div>
+                        <div>{`Restarts: ${restarts}`} </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="gamemode">Difficulty</label>
+                        <select
+                            id="gamemode"
+                            onChange={({ target }) => {
+                                console.log(target.value);
+                                setPokemonAmount(target.value);
+                            }}
+                            name="gamemode"
+                        >
+                            <option value={5}>Easy</option>
+                            <option value={10}>Normal</option>
+                            <option value={25}>Hard</option>
+                        </select>
+                        <button onClick={handleRestart}>Restart</button>
+                    </div>
                 </nav>
             </header>
+
             <PokemonCards
                 setPoints={setPoints}
                 pokemonAmount={pokemonAmount}
                 restartAmount={restarts}
+            />
+            <WinnerScreen
+                handleRestart={handleRestart}
+                pokemonAmount={pokemonAmount}
+                points={points}
             />
         </>
     );
