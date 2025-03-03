9af5aea869386c716a7b7260d1fc9b317c2aa726
R3ndrex <yacholohchtoli@gmail.com>
2025-03-03 14:37:27 +0200

    Change pokemonAmount to cardAmount, add input placeholder, add input validation,

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const [points, setPoints] = useState(0);
     const [restarts, setRestarts] = useState(0);
     let ref = useRef(0);
-    const [pokemonAmount, setPokemonAmount] = useState(5);
+    const [cardAmount, setCardAmount] = useState(5);
     const handleRestart = () => {
         setRestarts((prev) => ++prev);
         setPoints(0);
@@ -19,7 +19,6 @@ function App() {
                 <h1>Pokémon Memory Game</h1>
                 <nav>
                     <div>
-                        {" "}
                         <div>{`Points: ${points}`}</div>
                         <div>{`Restarts: ${restarts}`} </div>
                     </div>
@@ -29,11 +28,22 @@ function App() {
                             type="number"
                             name="pokemon-amount"
                             id="pokemon-amount"
+                            placeholder="Enter card amount"
                             ref={ref}
-                            defaultValue={pokemonAmount}
+                            defaultValue={cardAmount}
                         />
                         <button
-                            onClick={() => setPokemonAmount(ref.current.value)}
+                            onClick={() => {
+                                if (
+                                    Number(ref.current.value) < 3 ||
+                                    Number(ref.current.value) % 1 !== 0
+                                )
+                                    return console.error(
+                                        `Can't have this card amount`
+                                    );
+                                setCardAmount(Number(ref.current.value));
+                                setPoints(0);
+                            }}
                         >
                             Set Amount
                         </button>
@@ -41,17 +51,16 @@ function App() {
                     </div>
                 </nav>
             </header>
-
-            <PokemonCards
-                setPoints={setPoints}
-                pokemonAmount={pokemonAmount}
-                restartAmount={restarts}
-            />
-            <WinnerScreen
-                handleRestart={handleRestart}
-                pokemonAmount={pokemonAmount}
-                points={points}
-            />
+            <main>
+                <PokemonCards
+                    setPoints={setPoints}
+                    cardAmount={cardAmount}
+                    restartAmount={restarts}
+                />
+                {points === cardAmount && (
+                    <WinnerScreen handleRestart={handleRestart} />
+                )}
+            </main>
         </>
     );
 }
